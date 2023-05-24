f9abe67cb99575f18734eab4fe13eccfaa9135bb
MaduriMuppana <muppana.madhuri@gmail.com>
2023-05-24 11:47:45 -0500

    section-16-changes

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -134,7 +134,7 @@ const countriesContainer = document.querySelector('.countries');
 //encapusulated pieces of software that other peices of software can interact with them
 
 //THE MOST IMPORANT API  WHEN USING AJAX:
-//thne most important api that we are interested in when we use AJAX, and that are APIS that are like to
+//the most important api that we are interested in when we use AJAX, and that are APIS that are like to
 //call Online APIS
 
 //ONLINE APIS: an online api is essentially an application running on a web server, which receives requests
@@ -161,7 +161,7 @@ const countriesContainer = document.querySelector('.countries');
 //so AJAX stands for asynchronous javascript and XML, so the X there for XML, XML is a data format, which
 //used to be widely used to transmit data on web, however these days basically no API's uses XML data
 //anymore the term AJAX is just an old name that got very popular back in the day, and so its still used
-//today even thoght we dont use XML anymore, s
+//today even thoght we dont use XML anymore,
 
 //so instead most API's these days use the JSON data format, so JSON is the most popular data format today
 //because it is basically just a js obj but converted into a string, and therfore it is very easy to
@@ -188,11 +188,11 @@ Happy learning ✌️
 //-----------------------------------------------------------------------------
 //248: OUR FIRST AJAX CALL: XMLHTTP REQUEST:
 //in this lecture we gonna build a nice ui component, which contains data about a certain country, and this
-//data abt countries is gonna come fro a 3rd part online api
+//data abt countries is gonna come from a 3rd part online api
 
 //in js there are multiple ways of doing AJAX calls, lets starts with XML http request fn, we can do
 //const request = new XMLHttpRequest(); we call this fn then all we need to do for now is to store the result in a new var
-//this is the old school way of doing AJAX in js, nect we need a url to which we make the AJAX call
+//this is the old school way of doing AJAX in js, next we need a url to which we make the AJAX call
 //we can now say,request.open(), in here we need to pass in the type of request, the type of HTTP request
 //to get data is simple GET and 2nd we need a string containing the url to which the AJAX call should actually
 //be made,
@@ -205,7 +205,7 @@ Happy learning ✌️
 //api for certain country we use search by name we are going to use
 //https://restcountries.com/v3.1/name/{name}
 
-//with this we basically opened a request and now we also need to send it bu using request.send(), this
+//with this we basically opened a request and now we also need to send it by using request.send(), this
 //will now send now send off the request to this url
 
 //now inorder to get the result, we could'nt simply do somthing like this, set some variable to the result
@@ -235,7 +235,6 @@ Happy learning ✌️
 //   //converting JSON
 // });
 
-// //we call this fn and store it into a new variable
 // const request = new XMLHttpRequest();
 // //we need a url to which we make the ajax call
 // //with this we opened a request
@@ -271,23 +270,189 @@ Happy learning ✌️
 //for languages it is basically an array, what we want is the 1st el of languages and then the name, and the
 //same with the currencies an insert this into our html page
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
-request.send();
-request.addEventListener('load', function () {
-  console.log(this.responseText);
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-  const html = ` <article class="country">
+// //we call this fn and store it into a new variable
+/*
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  // //we need a url to which we make the ajax call
+  // //with this we opened a request
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  // //to send request
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText); // The data is in json
+    //converting JSON(json is realyy a big string of text)
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    //{name: {…}, tld: Array(1), cca2: 'PT', ccn3: '620', cca3: 'PRT', …}
+    //we have an obj with all the data about the portugal, this is an array containing one obj, so we can destructure
+
+    //we can built the card component, lets copy html from index, we can create a template literal and
+    //replace the data
+    const html = ` <article class="country">
   <img class="country__img" src="${data.flag}" />
   <div class="country__data">
-    <h3 class="country__name">COUNTRY</h3>
-    <h4 class="country__region">REGION</h4>
-    <p class="country__row"><span>👫</span>POP people</p>
-    <p class="country__row"><span>🗣️</span>LANG</p>
-    <p class="country__row"><span>💰</span>CUR</p>
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${+(
+      data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
   </div>
 </article>`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.getElementsByClassName.opacity = 1;
-});
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('india');
+*/
+//for languages it is basically an array, we want the 1st el of the languages and the name
+
+//we can reuse the same code to create an el like this for multiple countries, so we put all of this into
+//a fn, this one will take string as an i/p of a country, so instead of hardcording portugal we can use
+//country that we get as an i/p
+
+//now we can call this fn
+
+//now we have data about 2 countries, now by calling these 2 fn's twice, we will basically have 2 ajax calls
+//happening at the same time, so if we reaload the page here couple of times now then they might appear in
+//diff order, the reason for that is basically that the data arrives at slightly diff time, ecah time that
+//we are loading the page, so infact this realyy shows the non-blocking behaviour in action, so as we call
+//getCountryData('portugal'); with potugal, for the very 1st time , it sends of this request and then js
+//will moves on in the code right away, so it goes right here in the next line getCountryData('usa');, and
+//this ofcourse fires off another ajax call immediately way before the data of the portugal has actually
+//arrived, so gain we will have 2 ajax calls happening at the same time, and so whatever one arrives 1st
+//will then fire the load event 1st, and so the 1st one is to ajax call for the usa, well then the 1st
+//el thta is gonna be printed here will of course, be the one from usa, and only after that the data
+//for portugal arrives, and then this call bcak fn here is called with the data of portugal
+
+//now if we actually wanted these requests to be made in a specific, like predefined order,, then we would
+//basically have to chain the requests, which means to make the 2nd request, only after the 1sr request
+//has finished
+
+//-----------------------------------------------------------------------------
+//249: [OPTIONAL]: HOW WEB WORKS: REQUESTS AND RESPONSES:
+
+//client     ----------------->(request)   web server
+//           <-------------------(response)
+
+//whenever we try to access a web server, the browser which is client, sends a request to the server and the
+//server will then send back a resonse and that response contains the data or the web page that we requested
+
+//REQUEST RESONSE MODEL or CLIENT-SERVER ARCHITECTURE
+//this process works exact same way no matter if we are accessing an entire web page or just some data from
+//a web api, this whole process actually has a name and it is called request response model or client-server
+//architecture
+
+//lets use the ex of the url that we used in last lecture to get our countyr data
+
+//https://restcountries.com/v2/name/${country}
+//now every url gets an HTTP or HTTPS which is for the protocol that we used on this connection, then we have
+//a domain name, which is the restcountries.com and also after slash, we have a so-called (v2) resource that
+//we want to access, now this domain name restcountries.com is actually not the real address of the server
+//that we are trying to access, it is really just a nice name that is easy for us to remember, but what is
+//thta means is that we need a way of kind of converting the domain name to the real address of the server
+
+//and that happens through a so-called D-N-S
+//DNS(DOMAIN NAME SERVER): These are a special kind of servers, so they are basically like the phone books
+//of the internet, so the 1st step that happens when we access any web server is that the browser makes
+//a request to a DNS and this special server will then simply match the web address of the url to the
+//servers real IP address, this all happens through your internet service provider
+
+//the domain is not the real address and that a DNS will convert the domain to the real IP address, and then
+//after the real IP address has been sent back to the browser, we can finally call it
+
+//once we have the real ip address, a tcp socket is established b/w the browser and the server, and so they
+//are now finally connected, and this connection is typically kept alive fro th entire time that it takes
+//to transfer all the files of the website or all data
+
+//WHAT ARE TCP AND IP?
+//TCP: transmission control protocol
+//IP: internet protocol
+
+//they are communication protocols that define exactly how data travels across the web, they are basically
+//the internet's fundamental control systems, because again they are the ones who set the rules about how
+//data moves on the internet
+
+//now its time to finally make our request, and the request that we make is an HTTP request, where HTTP
+//stands for hypertext transfer protocol
+
+//after TCP/IP, HTTP is another communication protocol, a communication protocol is simply a sysytem of rules
+//thta allows us 2 or more parties to communicate
+
+//now in the case of HTTP, it's just a protocol that allows client and webserver to communicate, and thta
+//works by sending requests and resonse messages from client to the server and back, now a request msg
+//looks somthing like this
+
+// GET /rest/v2/alpha/PT HTTP/1.1  -----> start line ->  this contains the HTTP method that is used in
+//the request, and then the request target and then the HTTP version
+//HTTP method + request target + http version
+//so about the http methods there are many available, but the most important ones are GET, for simply
+//requesting data,  POST for sending data and PUT and PATCH, to basically modify data, so you will see that
+
+//HTTP request: to a server is not only for getting data, but we can also send data
+//Request data: This is where the server is told that we want to access the rest/v2/alpha resource, so we had
+//this in the url before and now it is simply sent as the target in the HTTP request, and so then the server
+//can figure out what to do with it, now if the target was empty, so of it was just a slash then we would
+//be accessing the website's route, which is just restcountries.eu in this example
+
+//then the next part of the requests or the request headers: which is just some info that we sent about the
+//equest itself, there are tons of standared different headers, like what browser is used to make the request
+//at what time, the user's language and many more
+// Host: www.google.com
+// user-agent: Mozilla/5.0
+// Accept-Language: en-US
+//<body>
+
+//now finally in the case where we are sending the data to the server, there will also be the request body,
+//and that body will contain the data that we're sending, for ex coming from an HTML FORM,
+//that is the HTTP request
+
+//it is not our developers who manually write these HTTP requests, there is also HTTPS
+
+//DIFFERENCE BETWEEN HTTP AND HTTPS:
+//HTTPS: is encrypted using TLS OR SSL, which are yet some other protocols, but besides that the behind
+//the HTTP requests and responses still applies to HTTPS
+
+//so our request is formed and now it hits the server, which will then be working on it until it has our
+//data or web page eady to send back, and once it is ready, it will send it back using, an HTTP response
+//and the HTTP response message actually looks quite similar to the request, so also with a start line
+//headers nd a body
+
+//now in this case the start line has besides the version also a status code and a message
+//HTTP/1.1 200 ok
+//HTTP VERSION + status code + status message
+//these are used to let the client know whether the request has been successful or failed, for example 200
+//means ok and the status code thta everyone knows is 404, which menas page not found, so that is where this
+//404 code, that everyone already know, comes from, then the respnose headers  are info about the esponse
+//itself, so just like before, and there are ton available and we can also make up our own actually
+//and finally the last part of the reosnse is again the body, which is present in most repsonses, and this
+//body usually contains the JSON data coming back from an API or HTML of the web page that we are requested
+//
+
+//we only just did one request to restcoutires.au and got one response back, thta is how it is going to work
+//when all we do is to access an API, however, if it's a web page that we are acceswsing, then there will be
+//many more requests and responses, and that is because when we do the 1st request, all we get back is just
+//the initial HTML file, that HTML file will then get scanned by the browser for all the assets that it needs
+//in order to build the entire web page like js, css, image files etc.., and then for ecah diff file, there
+//will be a new HTTP request made to the server, so basically this entire back and forth b/w client and server
+//happens for every single file that is included n the web page, however there can be mutilple requests
+//and responses happening at the same time, but the maount is still limited bcoz otherwise the connection
+//would start to slow down, but anyway, when all the files have finaaly arrived, then the web page can be
+//rendered in the browser, according to the HTML, css and js specifications
+
+//now as the final piece of the puzzle, let's talk about tcp/ip
+
+//TCP AND IP are the communication protocols that define how data travels across the web
+//the job of TCP is to break the requests and responses into 1000's of small chunks, called packets before
+//they are sent, once the small packets arrive at their final destination, tcp will reassemble all the
+//packets into the original request or repsonse, this is necessary so that each packet can take a diff
+//route through the internet, because this way the message arrives at the destination as quick as possible
+//which would not be possible if we sent the entire data simply as a big chunk, as a second part
+
+//th job of IP protocol is to actually send and route these packets through the internet, so it ensures
+//that they arrive at the destination they should go, using IP address on each packet
